a2bf2c057fbed8bfcfef55fbdfb5e085dc76f294
aqnstu <blinov.nstu@gmail.com>
2021-06-10 19:12:07 +0700

    поправил наименования методов

--- apiapp.py
+++ apiapp.py
@@ -7,7 +7,7 @@ import logging.config
 from db import crud, models, schemas
 from db.database import SessionLocal, engine
 from logger import CustomizeLogger
-from model import Document
+from model import Document, String
 from signer import get_jwt, to_base64_string
 
 
@@ -32,12 +32,12 @@ def get_db():
 
 @app.get("/")
 def read_root():
-    return {"message": "API для подписи для СС"}
+    return {"message": "API для подписи и получения данных для работы с суперсервисом [Поступи онлайн]"}
 
 
 @app.post("/api/utils/create-base64")
-def create_item(data: str = Body(..., embed=True)) -> str:
-    data_base64 = to_base64_string(data)
+def create_item(s: String) -> str:
+    data_base64 = to_base64_string(s.data)
     return {"data_base64": data_base64}
 
 
@@ -60,7 +60,7 @@ def read_subdivision_org(
 
 
 @app.get("/api/db/get-education-program")
-def read_subdivision_org(
+def read_education_program(
     skip: int = 0, limit: int = 100, db: Session = Depends(get_db)
 ):
     try:
@@ -72,7 +72,7 @@ def read_subdivision_org(
 
 
 @app.get("/api/db/get-campaign")
-def read_subdivision_org(
+def read_campaign(
     skip: int = 0, limit: int = 100, db: Session = Depends(get_db)
 ):
     data = crud.get_campaign(db, skip=skip, limit=limit)
@@ -80,7 +80,7 @@ def read_subdivision_org(
 
 
 @app.get("/api/db/get-cmp-achievement")
-def read_subdivision_org(
+def read_cmp_achievement(
     skip: int = 0, limit: int = 100, db: Session = Depends(get_db)
 ):
     data = crud.get_cmp_achievement(db, skip=skip, limit=limit)
@@ -88,7 +88,7 @@ def read_subdivision_org(
 
 
 @app.get("/api/db/get-admission-volume")
-def read_subdivision_org(
+def read_admission_volume(
     skip: int = 0, limit: int = 100, db: Session = Depends(get_db)
 ):
     data = crud.get_admission_volume(db, skip=skip, limit=limit)
@@ -96,7 +96,7 @@ def read_subdivision_org(
 
 
 @app.get("/api/db/get-distributed-admission-volume")
-def read_subdivision_org(
+def read_distributed_admission_volume(
     skip: int = 0, limit: int = 100, db: Session = Depends(get_db)
 ):
     data = crud.get_distributed_admission_volume(db, skip=skip, limit=limit)
@@ -104,7 +104,7 @@ def read_subdivision_org(
 
 
 @app.get("/api/db/get-competitive-group")
-def read_subdivision_org(
+def read_competitive_group(
     skip: int = 0, limit: int = 200, db: Session = Depends(get_db)
 ):
     data = crud.get_competitive_group(db, skip=skip, limit=limit)
@@ -112,7 +112,7 @@ def read_subdivision_org(
 
 
 @app.get("/api/db/get-competitive-group-program")
-def read_subdivision_org(
+def read_competitive_group_program(
     skip: int = 0, limit: int = 200, db: Session = Depends(get_db)
 ):
     data = crud.get_competitive_group_program(db, skip=skip, limit=limit)
@@ -120,7 +120,7 @@ def read_subdivision_org(
 
 
 @app.get("/api/db/get-competitive-benefit")
-def read_subdivision_org(
+def read_competitive_benefit(
     skip: int = 0, limit: int = 200, db: Session = Depends(get_db)
 ):
     data = crud.get_competitive_benefit(db, skip=skip, limit=limit)
@@ -128,7 +128,7 @@ def read_subdivision_org(
 
 
 @app.get("/api/db/get-entrance-test")
-def read_subdivision_org(
+def read_entrance_test(
     skip: int = 0, limit: int = 1000, db: Session = Depends(get_db)
 ):
     data = crud.get_entrance_test(db, skip=skip, limit=limit)
@@ -136,7 +136,7 @@ def read_subdivision_org(
 
 
 @app.get("/api/db/get-entrance-test-benefit")
-def read_subdivision_org(
+def read_entrance_test_benefit(
     skip: int = 0, limit: int = 15000, db: Session = Depends(get_db)
 ):
     data = crud.get_entrance_test_benefit(db, skip=skip, limit=limit)
@@ -144,7 +144,7 @@ def read_subdivision_org(
 
 
 @app.get("/api/db/get-entrance-test-location")
-def read_subdivision_org(
+def read_entrance_test_location(
     skip: int = 0, limit: int = 5000, db: Session = Depends(get_db)
 ):
     data = crud.get_entrance_test_location(db, skip=skip, limit=limit)
@@ -152,8 +152,8 @@ def read_subdivision_org(
 
 
 @app.get("/api/db/get-terms-admission")
-def read_subdivision_org(
-    skip: int = 0, limit: int = 5000, db: Session = Depends(get_db)
+def read_terms_admission(
+    skip: int = 0, limit: int = 100, db: Session = Depends(get_db)
 ):
     data = crud.get_terms_admission(db, skip=skip, limit=limit)
     return data
--- model.py
+++ model.py
@@ -6,3 +6,6 @@ from typing import Optional
 class Document(BaseModel):
     header: str
     payload: Optional[str] = None
+    
+class String(BaseModel):
+    data: str
